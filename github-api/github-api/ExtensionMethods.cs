ccc44f53ea4d2756e7b06593f32db2d632602d11
Henrique Andrade Cruz <henrique.andrade22@hotmail.com>
2024-04-04 16:48:01 -0300

    improve: input handle

---
+++ github-api/github-api/ExtensionMethods.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+namespace github_api;
+
+public static class ExtensionMethods
+{
+    public static string Replace(this string input, Regex regex, string newValue)
+    {
+        return regex.Replace(input, newValue);
+    }
+}
--- github-api/github-api/Program.cs
+++ github-api/github-api/Program.cs
@@ -1,10 +1,36 @@
 ﻿using github_api;
+using System.Text.RegularExpressions;
 
-Console.Write("Insira o usuário do GitHub: ");
+internal partial class Program
+{
+    private static async Task Main(string[] args)
+    {
+        string user;
 
-var user = Console.ReadLine();
-var usersDontFollowBack = await GitHubService.GetUsersDontFollowBack(user!);
+        do
+        {
+            Console.Write("Insira o usuário do GitHub: ");
 
-Console.WriteLine(usersDontFollowBack.Count());
-Console.WriteLine(string.Join("\n", usersDontFollowBack.Select(u => u.Login)));
-Console.ReadKey();
+            var input = Console
+                .ReadLine()!
+                .Replace(SpecialCharsRegex(), string.Empty)
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            user = input.FirstOrDefault()!;
+
+            if (string.IsNullOrWhiteSpace(user))
+            {
+                continue;
+            }
+
+            var usersDontFollowBack = await GitHubService.GetUsersDontFollowBack(user);
+
+            Console.WriteLine($"\nTotal: {usersDontFollowBack.Count()}\n");
+            Console.WriteLine(string.Join("\n", usersDontFollowBack.Select(u => u.Login)));
+        }
+        while (!string.IsNullOrWhiteSpace(user));
+    }
+
+    [GeneratedRegex("[^a-zA-Z0-9]")]
+    private static partial Regex SpecialCharsRegex();
+}
